09efd4c8d9519e6182598e85b4a84ea1ba687adc
bmigo <berniecmigo@gmail.com>
2023-02-08 09:51:01 -0500

    index.js and created template helper

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-const employee = new Employee();
+const fs = require('fs');
 
-//call function with employee
+const generateTeamMembers = require('./src/template'); //complete template.js
+const { default: inquirer } = require('inquirer');
+
+function runApp () {
+    //create functions with inquirer to prompt user
+    function makeTeam () {
+        inquirer.prompt(
+            [
+                {
+                    type:"list",
+                    message:"What kind of employee would you like to add?",
+                    name:"addEmployee",
+                    choices: ["Engineer", "Intern", "Manager", "No new employee"]
+                }
+            ]).then(function (userInput){
+                switch (userInput.addEmployee) {
+                    case "Engineer": addEngineer(); break;
+                    case "Intern": addIntern(); break;
+                    case "Manager": addManager(); break;
+                }
+            })
+    }
+    function addEngineer () {
+        //create inquirer prompt for engineer
+    }
+
+    function addIntern () {
+        //create inquirer prompt for intern
+    }
+
+    function addManager () {
+        //create inquirer prompt for manager
+    }
+
+
+}
---
+++ src/template.js
@@ -0,0 +1,3 @@
+//TO DO: Create the team profile: generateTeamMembers
+// generateEngineer = engineer, generateIntern = intern, generateManager = manager
+// return each employee as a card and then module export the team from generateTeamMembers and return html
