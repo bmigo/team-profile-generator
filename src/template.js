2551404e5b35ccd21da5f5c01b18f0f30cb7e48a
bmigo <berniecmigo@gmail.com>
2023-02-08 10:24:23 -0500

    notes

--- src/template.js
+++ src/template.js
@@ -3,5 +3,5 @@
 // return each employee as a card and then module export the team from generateTeamMembers and return html
 
 generateTeamMembers = teamMembers => {
-
+//generate a card for each type of employee
 };
